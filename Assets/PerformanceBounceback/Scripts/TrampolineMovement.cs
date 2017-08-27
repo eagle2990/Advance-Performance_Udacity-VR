2c627d8339c98e3a81b5c172494617530eb3985e
eagle2990 <eagle2990@gmail.com>
2017-08-27 18:25:25 -0500

    Fixed Update for Trampoline Movement

--- Assets/PerformanceBounceback/Scripts/TrampolineMovement.cs
+++ Assets/PerformanceBounceback/Scripts/TrampolineMovement.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrampolineMovement : MonoBehaviour {
-
+public class TrampolineMovement : MonoBehaviour
+{
     public Vector3 direction = Vector3.right;
     public float moveSpeed = 3.5f;
     public float moveTime = 3f;
     private float time;
-	
-	// Update is called once per frame
-	void Update () {
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         time += Time.deltaTime;
-        if(time > moveTime)
+        if (time > moveTime)
         {
             time = 0;
             direction = direction * -1;
         }
-        transform.position += direction * Time.deltaTime * moveSpeed;		
-	}
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + direction * Time.deltaTime * moveSpeed);
+    }
 }
